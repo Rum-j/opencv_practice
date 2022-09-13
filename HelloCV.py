d3f834c14db7bbc8a3509faa09e504aa1cbe90e5
Jieun Park <diwoon95@gmail.com>
2022-09-13 19:07:31 +0900

    example of cat image added

--- HelloCV.py
+++ HelloCV.py
@@ -1,9 +1,22 @@
+import sys
 import cv2
 
 
 def vcheck():
     return "hello, openCV" f"{cv2.__version__}"
 
+
+img = cv2.imread('cat.bmp')
+
+if img is None:  # 이미지를 불러올 수 없는 경우에 대한 방어
+    print('Image load failed')
+    sys.exit()
+
+cv2.namedWindow('image')
+cv2.imshow('image', img)
+cv2.waitKey()  # 키보드 입력이 들어올 때까지 기다리기
+
+cv2.destroyAllWindows()  # 기존의 창을 모두 닫기
 
 if __name__ == "__main__":  # pragma: no cover
     ret = vcheck()
--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ import HelloCV
 class MainTest(unittest.TestCase):
     def test_HelloCV(self):
         ret = HelloCV.vcheck()
-        self.assertEqual(ret, "hello, openCV4.6.0")
+        self.assertEqual(ret, "hello, openCV4.6.0")  # openCV version check
 
 
 if __name__ == "__main__":  # pragma: no cover
