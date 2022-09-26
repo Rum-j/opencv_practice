c5bde303440d19162f10feef157c12243deeacbc
Jieun Park <diwoon95@gmail.com>
2022-09-26 11:32:51 +0900

    slideshow with full screen with loop

--- slideshow.py
+++ slideshow.py
@@ -1,6 +1,7 @@
 from glob import glob
 import os
 import glob
+import cv2 
 
 
 file_list = os.listdir('./')
@@ -10,5 +11,22 @@ file_list = os.listdir('./')
 # glob으로 불러오기 
 img_files = glob.glob('./*.png') # 특정 패턴의 문자열로 되어있는 file name을 불러오기
 
+
 for f in img_files:
     print(f)
+
+
+cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+cv2.setWindowProperty('image', cv2.WND_PROP_FULLSCREEN
+                      cv2.WINDOW_FULLSCREEN)
+
+cnt = len(img_files) # 이미지 개수만큼 순환
+idx = 0
+while True:
+    img = cv2.imread(img_files[idx])
+    cv2.imshow('image', img)
+    if cv2.waitKey(1000) == 27: # 1초 기다리기, 하지만 esc 누르면 루프 그만
+        break
+    idx += 1
+    if idx >= cnt:
+        idx =0 # initialize
